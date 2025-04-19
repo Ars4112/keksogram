e065e7ec7488068ddf61b29c1bb161159d0e0908
Арсений <ars4112@gmail.com>
2025-04-19 21:00:25 +0300

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,11 @@ import "./data.js";
 import "./form.js";
 import "./effects.js";
 import { createUserPhotos } from "./photo.js";
-import "../nouislider/nouislider.js";
-import "../pristine/pristine.min.js"
+// import "../nouislider/nouislider.js";
+import "../pristine/pristine.min.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+	const noUiSlider = await import("/nouislider/nouislider.js");
+});
 
 createUserPhotos();
