4f92454b31af92c67e5bc52643c9ece463640efe
Арсений <ars4112@gmail.com>
2025-04-19 21:02:47 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,8 @@ export default defineConfig({
 	build: {
 		outDir: "../dist",
 		emptyOutDir: true,
-		// assetsInclude: ["**/*.css", "**/*.js"],
 	},
+  optimizeDeps: {
+    include: ['nouislider']
+  }
 });
