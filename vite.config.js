d57937e7f6078356d1da5404c850189d5f5f8994
Арсений <ars4112@gmail.com>
2025-04-19 22:57:15 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
 import path from "path";
-import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -9,7 +8,7 @@ export default defineConfig({
   root: path.resolve(__dirname, "src"),
   base: "/keksogram/",
   build: {
-    outDir: path.resolve(__dirname, 'dist'),
+    outDir: "../dist",
     emptyOutDir: true,
   },
 });
