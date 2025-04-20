3649449a441fb802a481174442fdde9dcb887cb5
Арсений <ars4112@gmail.com>
2025-04-20 18:54:49 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -12,24 +12,23 @@ export default defineConfig({
 	build: {
 		outDir: "../dist",
 		emptyOutDir: true,
-    assetsDir: 'photos',
 	},
-	plugins: [
-		viteImagemin({
-			gifsicle: { optimizationLevel: 3 },
-			mozjpeg: { quality: 75 },
-			optipng: { optimizationLevel: 5 },
-			svgo: {
-				plugins: [{ name: "removeViewBox" }, { name: "removeEmptyAttrs", active: false }],
-			},
-		}),
-		viteStaticCopy({
-			targets: [
-				{
-					src: path.resolve(__dirname, "src/photos/*.jpg"),
-					dest: "photos",
-				},
-			],
-		}),
-	],
+	// plugins: [
+	// 	viteImagemin({
+	// 		gifsicle: { optimizationLevel: 3 },
+	// 		mozjpeg: { quality: 75 },
+	// 		optipng: { optimizationLevel: 5 },
+	// 		svgo: {
+	// 			plugins: [{ name: "removeViewBox" }, { name: "removeEmptyAttrs", active: false }],
+	// 		},
+	// 	}),
+	// 	viteStaticCopy({
+	// 		targets: [
+	// 			{
+	// 				src: path.resolve(__dirname, "src/photos/*.jpg"),
+	// 				dest: "photos",
+	// 			},
+	// 		],
+	// 	}),
+	// ],
 });
