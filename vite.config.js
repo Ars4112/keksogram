bf31a6c2b039a0f1f371ff6eb1ca1d0dd0051fb1
Арсений <ars4112@gmail.com>
2025-04-20 17:53:39 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,35 @@
 import { defineConfig } from "vite";
 import { fileURLToPath } from "url";
 import path from "path";
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { viteStaticCopy } from "vite-plugin-static-copy";
+import viteImagemin from "vite-plugin-imagemin";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  root: path.resolve(__dirname, "src"),
-  base: "/keksogram/",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
-  // plugins: [
-  //   viteStaticCopy({
-  //     targets: [
-  //       {
-  //         src: 'src/photos/*.{jpg,png,webp}',
-  //         dest: 'photos'
-  //       }
-  //     ]
-  //   })
-  // ]
+	root: path.resolve(__dirname, "src"),
+	base: "/keksogram/",
+	publicDir: true,
+	build: {
+		outDir: "../dist",
+		emptyOutDir: true,
+	},
+	plugins: [
+		viteImagemin({
+			gifsicle: { optimizationLevel: 3 },
+			mozjpeg: { quality: 75 },
+			optipng: { optimizationLevel: 5 },
+			svgo: {
+				plugins: [{ name: "removeViewBox" }, { name: "removeEmptyAttrs", active: false }],
+			},
+		}),
+		viteStaticCopy({
+			targets: [
+				{
+					src: path.resolve(__dirname, "src/photos/*.jpg"),
+					dest: "photos",
+				},
+			],
+		}),
+	],
 });
