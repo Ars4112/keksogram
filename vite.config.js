dcb72ce410b7df12d03616c266f6b7ed4ad196e8
Арсений <ars4112@gmail.com>
2025-04-20 18:35:54 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,10 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
 	root: path.resolve(__dirname, "src"),
 	base: "/keksogram/",
-	publicDir: true,
 	build: {
 		outDir: "../dist",
 		emptyOutDir: true,
+    assetsDir: 'photos',
 	},
 	plugins: [
 		viteImagemin({
